6381de8366eac431adaceb4fae059687344e5c89
Isabel Laurenceau <isalau@ufl.edu>
2019-10-20 20:39:19 -0400

    Updated init and start_link of nodes

--- project3/project3.exs
+++ project3/project3.exs
@@ -7,24 +7,27 @@ defmodule MAINPROJ do
     # # number of Requests
     # numRequests = String.to_integer(Enum.at(argv, 1))
 
-    # Range of GenServers
-    rng = Range.new(1, numNodes)
-
     # Starting the dynamic Server
     {:ok, _pid} = TAPESTRY.start_link(1)
 
     ################# CREATES NUMBER OF NODES ######################
+    # Range of GenServers
+    rng = Range.new(1, numNodes)
 
-    ################# ADDS NODES TO TAPESTRY ######################
-    ################# ADSS NODES TO SUPERVISOR ######################
     for x <- rng do
-      neighbor_map = Full_topology.full_topology(x, rng)
-      IO.puts("Child #{x} starting")
-      TAPESTRY.start_child(x, numRequests, neighbor_map)
+      TAPESTRY.start_child(x, numRequests)
       IO.puts("Child #{x} started")
     end
 
-    ################# START ALL NODES ######################
+    ################# CREATE OVERLAY NETWORK  ######################
+    children = DynamicSupervisor.which_children(DySupervisor)
+
+    for x <- children do
+      {_, childPid, _, _} = x
+      TAPNODE.addToTapestry(childPid)
+    end
+
+    ################# SEND FIRST REQUEST FROM ALL NODES ######################
   end
 end
 
@@ -36,53 +39,45 @@ defmodule TAPESTRY do
     {:ok, _pid} = DynamicSupervisor.start_link(__MODULE__, index, name: __MODULE__)
   end
 
-  def start_child(index,numRequests,neighbor_map) do
+  def start_child(index, numRequests) do
     IO.puts("DynamicSupervisor adding #{index} child")
-    child_spec = Supervisor.child_spec({TAPNODE, [index,numRequests,neighbor_map]}, id: index, restart: :temporary)
+
+    child_spec =
+      Supervisor.child_spec({TAPNODE, [index, numRequests]},
+        id: index,
+        restart: :temporary
+      )
+
     {:ok, _child} = DynamicSupervisor.start_child(__MODULE__, child_spec)
   end
 
   def init(_index) do
     DynamicSupervisor.init(strategy: :one_for_one)
   end
-
-  #Tapestry and application handlers occurs through three primary calls (
-  # 1) DELIVER( G, AiD, Msg): Invoked on incoming messages destined for the local node. This is asynchronous and returns immediately. The application generates further events by invoking ROUTE().
-
-  #2) FORWARD( G, AiD, Msg): Invoked on incoming upcall-enabled messages. The application must call ROUTE() in order to forward this message on.
-
-  #3) ROUTE( G, AiD, Msg, NextHopNode): Invoked by the application handler to forward a message on to NextHopNode
 end
 
 defmodule TAPNODE do
   use GenServer
 
-  def start_link([index, numRequests,neighbor_map]) do
-    #Tapestry currently uses an identifier space of 160-bit values
-    #Tapestry assumes nodeIDs and GUIDs are roughly evenly distributed in the namespace, which can be achieved by using a secure hashing algorithm like SHA-1
+  def start_link([index, numRequests]) do
+    # -	Node N requests a new ID new_id
+
+    # Tapestry currently uses an identifier space of 160-bit values
+    # Tapestry assumes nodeIDs and GUIDs are roughly evenly distributed in the namespace, which can be achieved by using a secure hashing algorithm like SHA-1
     random_number = :rand.uniform(10000)
     sha = :crypto.hash(:sha, "#{random_number}")
-    nodeID = sha |> Base.encode16
-    IO.inspect(nodeID, label: "sha 1 output")
-
-    # find where you belong
-    # use the same algorithm as to find where an item would be in the network
-    TAPNODE.routeNode(self(),nodeID)
-
-    # update your neighbor_map (& neighbors update theirs)
-    IO.inspect(neighbor_map, label: "Its #{index} here in with sha1 TAPNODE with #{numRequests} requests and neighbor map")
-    {:ok, _pid} = GenServer.start_link(__MODULE__, {index, numRequests,neighbor_map}, name: :"#{index}")
+    new_id = sha |> Base.encode16()
+    IO.inspect(new_id, label: "sha 1 output")
+    numRequestToSend = numRequests
+    # neighborMap = empty list
+    # ROUTER = ??
+    # DYNAMIC_NODE_MANAGEMENT == ?
+    {:ok, _pid} = GenServer.start_link(__MODULE__, {index, new_id, numRequestToSend})
   end
 
   @impl true
-  def init({index, numRequests,neighbor_map}) do
-    #send a request to all your neighbors
-    # serverTo = Enum.at(neighbor_map, 0)
-    # serverFrom = self()
-    # msg = "testMsg"
-
-    # TAPNODE.sendMessageFunction(serverTo,serverFrom, msg)
-    {:ok, {index, numRequests,neighbor_map}}
+  def init({index, new_id, numRequestToSend}) do
+    {:ok, {index, new_id, numRequestToSend}}
   end
 
   # Server
@@ -94,34 +89,34 @@ defmodule TAPNODE do
   end
 
   # CLient
-  def sendMessageFunction(serverTo,serverFrom, msg) do
-    IO.inspect(serverTo, label: "in client sendMessageFunction serverTo" )
+  def sendMessageFunction(serverTo, serverFrom, msg) do
+    IO.inspect(serverTo, label: "in client sendMessageFunction serverTo")
     GenServer.call(serverTo, {:receiveMsg, {serverFrom, msg}})
   end
 
-  #Client
+  # Client
   def routeNode(pID, nodeID) do
     # A node N has a neighbor map with multiple levels, where each level contains links to nodes matching a prefix up to a digit position in the ID, and contains a number of entries equal to the ID’s base.
     # The primary ith entry in the jth level is the ID and location of the closest node that begins with prefix (N, j-1) + i
   end
 
-end
-
-defmodule Full_topology do
-  def full_topology(node_num, rng) do
-    # Produce a list of neighbors for the given specific node
-    main_node = node_num
-    nebhrs = Enum.filter(rng, fn x -> x != main_node end)
-    _nl = Enum.map(nebhrs, fn x -> :"#{x}" end)
+  # Client
+  def addToTapestry(childPid) do
+    state = :sys.get_state(childPid)
+    # state = index, new_id, numRequestToSend
+    index = elem(state, 0)
+    new_id = elem(state, 1)
+    numRequestToSend = elem(state, 2)
+    IO.inspect(new_id, label: "#{index}'s new_id is")
   end
 end
 
-#Take command line arguments
+# Take command line arguments
 arguments = System.argv()
 
-#Make them into integers
-numNodes = String.to_integer(Enum.at(arguments,0))
-numRequests = String.to_integer(Enum.at(arguments,1))
+# Make them into integers
+numNodes = String.to_integer(Enum.at(arguments, 0))
+numRequests = String.to_integer(Enum.at(arguments, 1))
 
-#Pass the integers to Actor 1
+# Pass the integers to Actor 1
 MAINPROJ.main(numNodes, numRequests)
